f26455fcd5055fc7458efd5c137210e99bb5b251
Vaishnavi Kamal Prasad <vaishnavikprasad@gmail.com>
2022-02-06 21:11:54 +0530

    Icons added

--- lib/ui/config/constants/peer_gig_icons.dart
+++ lib/ui/config/constants/peer_gig_icons.dart
@@ -1,1 +1,15 @@
-class PeerGigIcons {}
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class PeerGigIcons {
+  IconData home = Icons.home;
+  IconData backArrow = Icons.arrow_back_ios;
+  IconData search = Icons.search;
+  IconData calendar = Icons.calendar_today_rounded;
+  IconData myAccount = Icons.account_circle_outlined;
+  IconData addPost = Icons.post_add_rounded;
+  IconData viewPosts = Icons.article_outlined;
+  IconData clock = Icons.access_alarm;
+  IconData follow = Icons.person_add_alt_outlined;
+  IconData following = Icons.person_outline;
+}
