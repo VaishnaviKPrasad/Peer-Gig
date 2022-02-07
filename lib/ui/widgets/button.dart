7fd912c488adfe5b46a6db909c00a3d089e5f11a
SamridhiSethi13 <samridhisethi13@gmail.com>
2022-02-07 22:34:36 +0530

    made widget classes

--- lib/ui/widgets/button.dart
+++ lib/ui/widgets/button.dart
@@ -2,11 +2,16 @@ import '../config/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Widget pgButton(txt)
+class CustomButton extends StatelessWidget
 {
+  final String? txt;
+  const CustomButton({ Key? key, this.txt }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () {}, 
-      child: Text(txt, 
+      child: Text(txt!, 
         style: GoogleFonts.mallanna(
           color : AppColors.primaryDark,
           backgroundColor : AppColors.primaryLight,
@@ -16,4 +21,5 @@ Widget pgButton(txt)
         textAlign: TextAlign.center,
       ),
     );
+  }
 }
--- lib/ui/widgets/dropDown.dart
+++ lib/ui/widgets/dropDown.dart
@@ -2,12 +2,17 @@ import '../config/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 
-Widget dropDownButton()
+class CustomdropDownButton extends StatelessWidget
 {
-  return IconButton(
-    onPressed: () {},
-    icon: const Icon(Icons.arrow_drop_down,),
-    color: AppColors.primaryDark,
-    iconSize: 12,
-  );
+  const CustomdropDownButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {},
+      icon: const Icon(Icons.arrow_drop_down,),
+      color: AppColors.primaryDark,
+      iconSize: 12,
+    );
+  }
 }
