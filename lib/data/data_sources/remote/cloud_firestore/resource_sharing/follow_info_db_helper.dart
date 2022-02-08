0e93dff8d6e1d17add3f0c7fd845461040d78820
SamridhiSethi13 <samridhisethi13@gmail.com>
2022-02-08 17:26:05 +0530

    added methods for following

--- lib/data/data_sources/remote/cloud_firestore/resource_sharing/follow_info_db_helper.dart
+++ lib/data/data_sources/remote/cloud_firestore/resource_sharing/follow_info_db_helper.dart
@@ -28,4 +28,29 @@ class FollowInfoDBHelper {
         .get()
         .then((docSnap) => docSnap.data()['followers'].add(newFollowerUserId));
   }
+
+  // get the count of following
+  Future<int> getFollowingCount(String userId) async {
+    return await followInfoCollection
+        .doc(userId)
+        .get()
+        .then((docSnap) => docSnap.data()['following'].length);
+  }
+
+  // get following of a user
+  Future<List<String>> getFollowing(String userId) async {
+    return await followInfoCollection
+        .doc(userId)
+        .get()
+        .then((docSnap) => docSnap.data()['following']);
+  }
+
+  // add a new following for a user
+  Future<void> addNewFollowing(
+      {String? userId, String? newFollowingUserId}) async {
+    return await followInfoCollection
+        .doc(userId)
+        .get()
+        .then((docSnap) => docSnap.data()['following'].add(newFollowingUserId));
+  }
 }
