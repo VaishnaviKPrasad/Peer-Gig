943c3243fef86e0f22f3fbc84003263a1c0c1dcf
Sargam-Agarwal <sargam123india@gmail.com>
2022-02-08 16:56:54 +0530

    made some changes in the db helper classes of resource sharing

--- lib/data/data_sources/remote/cloud_firestore/resource_sharing/home_feed_posts_db_helper.dart
+++ lib/data/data_sources/remote/cloud_firestore/resource_sharing/home_feed_posts_db_helper.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import './single_post_info_db_helper.dart';
 
 class HomeFeedPostsDBHelper {
   final CollectionReference homeFeedPostsCollection =
@@ -16,7 +17,8 @@ class HomeFeedPostsDBHelper {
 
   // add the new post shared by one of the peers whom a user follows in the user's home feed
   Future<void> addNewHomeFeedPost(
-      {@required String? userId, @required String? postId}) async {
+      {@required String? userId, @required Map<String, Object>? map}) async {
+    String postId = await SinglePostInfoDBHelper().addNewPost(map);
     await homeFeedPostsCollection
         .doc(userId)
         .get()
