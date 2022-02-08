558c4048f325be382ff6b9a36db64fe8df26ea0c
SamridhiSethi13 <samridhisethi13@gmail.com>
2022-02-08 17:33:28 +0530

    reordered followInfo methods

--- lib/data/data_sources/remote/cloud_firestore/resource_sharing/follow_info_db_helper.dart
+++ lib/data/data_sources/remote/cloud_firestore/resource_sharing/follow_info_db_helper.dart
@@ -12,12 +12,28 @@ class FollowInfoDBHelper {
         .then((docSnap) => docSnap.data()['followers'].length);
   }
 
+  // get the count of following
+  Future<int> getFollowingCount(String userId) async {
+    return await followInfoCollection
+        .doc(userId)
+        .get()
+        .then((docSnap) => docSnap.data()['following'].length);
+  }
+
   // get followers of a user
   Future<List<String>> getFollowers(String userId) async {
     return await followInfoCollection
         .doc(userId)
         .get()
         .then((docSnap) => docSnap.data()['followers']);
+  }
+
+  // get following of a user
+  Future<List<String>> getFollowing(String userId) async {
+    return await followInfoCollection
+        .doc(userId)
+        .get()
+        .then((docSnap) => docSnap.data()['following']);
   }
 
   // add a new follower for a user
@@ -27,22 +43,6 @@ class FollowInfoDBHelper {
         .doc(userId)
         .get()
         .then((docSnap) => docSnap.data()['followers'].add(newFollowerUserId));
-  }
-
-  // get the count of following
-  Future<int> getFollowingCount(String userId) async {
-    return await followInfoCollection
-        .doc(userId)
-        .get()
-        .then((docSnap) => docSnap.data()['following'].length);
-  }
-
-  // get following of a user
-  Future<List<String>> getFollowing(String userId) async {
-    return await followInfoCollection
-        .doc(userId)
-        .get()
-        .then((docSnap) => docSnap.data()['following']);
   }
 
   // add a new following for a user
