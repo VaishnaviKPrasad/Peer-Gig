ed0d1c6c65acb364cfca9fcf06b9fd55fb159a48
Vaishnavi Kamal Prasad <vaishnavikprasad@gmail.com>
2022-02-10 19:26:01 +0530

    Made required changes

--- lib/domain/repositories/slot_booking/appointment_repository_base.dart
+++ lib/domain/repositories/slot_booking/appointment_repository_base.dart
@@ -1,7 +1,15 @@
 import '../../entities/slot_booking/appointment.dart';
 
 abstract class AppointmentRepositoryBase {
-  Future<void> addAppointment(Appointment appointment);
-  Future<void> deleteAppointment(String appointmentId);
-  Future<Appointment> getAppointment(String appointmentId);
+  //Future<Appointment> getAppointment(String appointmentId);
+  //Future<void> addAppointment(Appointment appointment);
+  //Future<void> deleteAppointment(String appointmentId);
+
+  Future<Appointment> getFifteenMinsAppointments(String appointmentId);
+  Future<void> addFifteenMinsAppointment(Appointment appointment);
+  Future<void> deleteFifteenMinsAppointment(String appointmentId);
+
+  Future<Appointment> getMockInterviewAppointments(String appointmentId);
+  Future<void> addMockInterviewAppointment(Appointment appointment);
+  Future<void> deleteMockInterviewAppointment(String appointmentId);
 }
