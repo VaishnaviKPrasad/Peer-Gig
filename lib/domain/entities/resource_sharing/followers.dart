edc9ccf9b1d72cad6dc15cb01ee17fc95c6eec97
Sargam-Agarwal <sargam123india@gmail.com>
2022-02-10 12:51:58 +0530

    coded up the followers and following model class

--- lib/domain/entities/resource_sharing/followers.dart
+++ lib/domain/entities/resource_sharing/followers.dart
@@ -1,1 +1,11 @@
-class Followers {}
+import 'package:flutter/foundation.dart';
+
+class Followers {
+  final int? count;
+  final List<String>? followers;
+
+  Followers({
+    @required this.count,
+    @required this.followers,
+  });
+}
--- lib/domain/entities/resource_sharing/following.dart
+++ lib/domain/entities/resource_sharing/following.dart
@@ -1,1 +1,11 @@
-class Following {}
+import 'package:flutter/foundation.dart';
+
+class Following {
+  final int? count;
+  final List<String>? followings;
+
+  Following({
+    @required this.count,
+    @required this.followings,
+  });
+}
