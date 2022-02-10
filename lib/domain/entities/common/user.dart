66a599681913ce65e39562c794b9805ae1ac5cfb
Sargam-Agarwal <sargam123india@gmail.com>
2022-02-10 12:48:43 +0530

    coded up the post model class

--- lib/domain/entities/common/user.dart
+++ lib/domain/entities/common/user.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 
 class User {
   final String? userId;
--- lib/domain/entities/resource_sharing/post.dart
+++ lib/domain/entities/resource_sharing/post.dart
@@ -1,1 +1,13 @@
-class Post {}
+import 'package:flutter/foundation.dart';
+
+class Post {
+  final String? body;
+  final String? postedBy;
+  final int? datePosted;
+
+  Post({
+    @required this.body,
+    @required this.postedBy,
+    @required this.datePosted,
+  });
+}
