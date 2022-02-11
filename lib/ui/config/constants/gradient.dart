fb2048f7cc80f27bce9cb6648dfa991c3057bad2
SamridhiSethi13 <samridhisethi13@gmail.com>
2022-02-11 17:32:43 +0530

    added SplashScreenGradient

--- lib/ui/config/constants/gradient.dart
+++ lib/ui/config/constants/gradient.dart
@@ -1,3 +1,22 @@
-class SplashScreenGradient {}
+//class SplashScreenGradient {}
+
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class SplashScreenGradient extends StatelessWidget {
+  const SplashScreenGradient({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          colors: [AppColors.primary!, AppColors.highlightColor!],
+          center: Alignment.center,
+        )
+      )
+    );
+  }
+}
 
 class AppScreenGradient {}
