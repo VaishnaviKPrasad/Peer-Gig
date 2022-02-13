da4f3af17b5ea920d8aa4c33f2f9061450dbd166
Vaishnavi Kamal Prasad <vaishnavikprasad@gmail.com>
2022-02-13 17:23:27 +0530

    Added AppScreenGradient

--- lib/ui/config/constants/gradient.dart
+++ lib/ui/config/constants/gradient.dart
@@ -1,3 +1,19 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
 class SplashScreenGradient {}
 
-class AppScreenGradient {}
+class AppScreenGradient extends StatelessWidget {
+  const AppScreenGradient({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          colors: [AppColors.primary!, AppColors.secondary!],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        )),
+      ));
+}
