c739a64ea19f379c9fc70f402ecf0fc72d6a12bc
Sargam-Agarwal <sargam123india@gmail.com>
2022-02-17 00:06:28 +0530

    added the coded for google sign in

--- lib/domain/repositories/authentication/auth_repository_base.dart
+++ lib/domain/repositories/authentication/auth_repository_base.dart
@@ -1,13 +1,5 @@
-import 'package:flutter/foundation.dart';
-
-import '../../entities/common/user.dart';
-
 abstract class AuthRepositoryBase {
-  Future<void> signUpWithEmailAndPassword(
-      {@required String? email,
-      @required String? password,
-      @required User? user});
+  Future<void> signUpWithGoogle();
 
-  Future<void> signInWithEmailAndPassword(
-      {@required String? email, @required String? password});
+  Future<void> signInWithGoogle();
 }
