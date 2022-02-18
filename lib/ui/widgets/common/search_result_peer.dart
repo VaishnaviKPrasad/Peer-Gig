488840fe6c5d91411618671c1d5053ee9e0bccdf
Vaishnavi Kamal Prasad <vaishnavikprasad@gmail.com>
2022-02-18 15:48:02 +0530

    Added Dp+Username component

--- lib/ui/widgets/common/search_result_peer.dart
+++ lib/ui/widgets/common/search_result_peer.dart
@@ -1,9 +1,19 @@
+import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../../config/constants/colors.dart';
 
 class SearchResultPeer extends StatelessWidget {
-  const SearchResultPeer(String s, {Key? key}) : super(key: key);
+  final Url? dpUrl;
+  final String? title;
+  final String? subtitle;
+
+  const SearchResultPeer(
+      {Key? key,
+      @required this.dpUrl,
+      @required this.title,
+      @required this.subtitle})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +25,7 @@ class SearchResultPeer extends StatelessWidget {
             radius: 26,
             child: const CircleAvatar(
                 backgroundImage: NetworkImage(
-                  "https://t4.ftcdn.net/jpg/02/79/66/93/360_F_279669366_Lk12QalYQKMczLEa4ySjhaLtx1M2u7e6.jpg",
+                  "",
                 ),
                 radius: 25)),
         ListTile(
@@ -24,11 +34,11 @@ class SearchResultPeer extends StatelessWidget {
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
           title: Text(
-            'XYZ name',
+            title!,
             style: GoogleFonts.mallanna(
                 color: AppColors.primaryDark, fontSize: 20),
           ),
-          subtitle: Text('Data Engineering Intern @Oracle',
+          subtitle: Text(subtitle!,
               style: GoogleFonts.mallanna(
                   color: AppColors.primaryDark, fontSize: 18)),
         )
