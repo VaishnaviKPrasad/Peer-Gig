02b95e10832a024aac415e3d1cac3c1dbb61258a
Vaishnavi Kamal Prasad <vaishnavikprasad@gmail.com>
2022-02-20 21:56:29 +0530

    Added title in account details listtile

--- lib/ui/widgets/common/account_details.dart
+++ lib/ui/widgets/common/account_details.dart
@@ -6,7 +6,12 @@ import 'package:peer_gig/ui/config/constants/peer_gig_icons.dart';
 class AccountDetails extends StatelessWidget {
   final String? txt;
   final bool? editable;
-  const AccountDetails({Key? key, @required this.txt, @required this.editable})
+  final String? title;
+  const AccountDetails(
+      {Key? key,
+      @required this.txt,
+      @required this.editable,
+      @required this.title})
       : super(key: key);
 
   @override
@@ -28,7 +33,12 @@ class AccountDetails extends StatelessWidget {
           tileColor: AppColors.primaryLight,
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-          title: Text(txt!,
+          title: Text(title!,
+              style: GoogleFonts.mallanna(
+                  color: AppColors.primaryDark,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold)),
+          subtitle: Text(txt!,
               style: GoogleFonts.mallanna(
                   color: AppColors.primaryDark, fontSize: 18)));
     }
