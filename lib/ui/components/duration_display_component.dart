1e8d99120a047ea19b6f4e4a757808f1fb13f137
SamridhiSethi13 <samridhisethi13@gmail.com>
2022-02-22 04:02:45 +0530

    modified durationDisplayComponent

--- lib/ui/components/duration_display_component.dart
+++ lib/ui/components/duration_display_component.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:peer_gig/ui/widgets/common/text_display.dart';
 import '../config/constants/colors.dart';
+import '../config/constants/peer_gig_icons.dart';
+import '../widgets/common/text_display.dart';
 
 // ignore: must_be_immutable
 class DurationDisplayComponent extends StatelessWidget {
@@ -10,16 +12,52 @@ class DurationDisplayComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Column(
       children: [
-        TextDisplay(txt: startTime.toString().substring(0,2)+":"+startTime.toString().substring(2)), 
-        Icon(
-          Icons.horizontal_rule_rounded,
-          color: AppColors.primaryLight,
-          size: 20.0,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextDisplay(txt: startTime.toString().substring(0,2)+":"+startTime.toString().substring(2)), 
+            Icon(
+              Icons.horizontal_rule_rounded,
+              color: AppColors.primaryLight,
+              size: 20.0,
+            ),
+            TextDisplay(txt: endTime.toString().substring(0,2)+":"+endTime.toString().substring(2)),
+          ],
         ),
-        TextDisplay(txt: endTime.toString().substring(0,2)+":"+endTime.toString().substring(2)),
+        const SizedBox(height: 20.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Stack(
+              children: [
+                const TextDisplay(txt: '-Select Date-'),
+                Positioned(
+                  right: 10.0, 
+                  child: Icon(
+                    PeerGigIcons.calendar,
+                    size: 20.0,
+                    color: AppColors.primaryLight,
+                  )
+                )
+              ]
+            ),
+            Stack(
+              children: [
+                const TextDisplay(txt: '-Select Time-'),
+                Positioned(
+                  right: 10.0, 
+                  child: Icon(
+                    PeerGigIcons.clock,
+                    size: 20.0,
+                    color: AppColors.primaryLight,
+                  )
+                )
+              ]
+            )
+          ]
+        )
       ],
     );
   }
