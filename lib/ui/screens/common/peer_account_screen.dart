0ea13b6353891f206e21ee3aa1e597520f1d8dcf
Sargam-Agarwal <sargam123india@gmail.com>
2022-03-24 23:36:39 +0530

    coded up the view peer's account screen

--- lib/ui/screens/common/peer_account_screen.dart
+++ lib/ui/screens/common/peer_account_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:peer_gig/ui/screens/common/peer_about_tab_screen.dart';
+import 'package:peer_gig/ui/screens/slot_booking/fifteen_mins_slot_booking_screen.dart';
 import 'package:peer_gig/ui/widgets/common/custom_app_bar.dart';
 import 'package:peer_gig/ui/widgets/common/custom_tab_bar.dart';
+
+import '../slot_booking/mock_interview_slot_booking_screen.dart';
 
 class PeerAccountScreen extends StatefulWidget {
   static const routeName = '/peerAccountScreen';
@@ -28,6 +32,29 @@ class _PeerAccountScreenState extends State<PeerAccountScreen> {
             ],
             length: 3,
           ),
+        ),
+        body: const TabBarView(
+          children: [
+            PeerAboutTabScreen(),
+            FifteenMinsSlotBookingScreen(
+                dpUrl:
+                    "https://1.bp.blogspot.com/-gz3ECbOByh0/YSyMST7rT_I/AAAAAAAAWYs/stFJ90JJpx0v3ewmmK6HMYiUmIvpNMriACLcBGAsYHQ/s1041/girl-cartoon-cute-dp-images%2B%25281%2529.jpg",
+                username: "Sargam Agarwal",
+                headline:
+                    "Summer Analyst @Goldman Sachs'22 | MLH Prep Fellow'21 | WeTech Goldman Sachs Scholar'21 | vGHC'21| Flutter App Developer",
+                isWeekends: "blah blah",
+                startTime: 1700,
+                endTime: 1800),
+            MockInterviewSlotBookingScreen(
+                dpUrl:
+                    "https://1.bp.blogspot.com/-gz3ECbOByh0/YSyMST7rT_I/AAAAAAAAWYs/stFJ90JJpx0v3ewmmK6HMYiUmIvpNMriACLcBGAsYHQ/s1041/girl-cartoon-cute-dp-images%2B%25281%2529.jpg",
+                title: "Sargam Agarwal",
+                subtitle:
+                    "Summer Analyst @Goldman Sachs'22 | MLH Prep Fellow'21 | WeTech Goldman Sachs Scholar'21 | vGHC'21| Flutter App Developer",
+                isWeekends: "blah blah",
+                startTime: 1700,
+                endTime: 1800),
+          ],
         ),
       ),
     );
